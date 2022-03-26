e5826990db767eaa66eabcee4ea1f6e2ee6736ee
Moises <moisesvasquezca@gmail.com>
2022-03-26 14:15:13 -0400

    Added Requirements file

--- tsc-qrcode.py
+++ tsc-qrcode.py
@@ -8,9 +8,9 @@ from PIL import Image
 st.title('The Source Church QR Code Generator App')
 
 url = st.sidebar.text_input("URL", value="", placeholder="Paste URL Here")
-source = slugify(st.sidebar.text_input("Source", value="", placeholder="Ex: Email Campaing"))
-medium = slugify(st.sidebar.text_input("Medium", value="qr-code", placeholder="Default is QR-Code"))
-name = slugify(st.sidebar.text_input("Name", value="", placeholder="Give this campaing a name"))
+source = slugify(st.sidebar.text_input("Source", value="", placeholder="Ex: Flyer, Door Hanger, Email Campaing"))
+medium = slugify(st.sidebar.text_input("Medium", value="QR Code", placeholder="Where is this URL going to be used?"))
+name = slugify(st.sidebar.text_input("Name", value="", placeholder="What is this Campaing Promoting?"))
 
 track = st.sidebar.checkbox('Track this URL')
 qr_size = st.sidebar.slider('QR Code Size', min_value = 6, max_value = 12, value = 6)
@@ -33,4 +33,4 @@ qr.png('tsc_qr.png', scale = qr_size)
 image = Image.open('tsc_qr.png')
  
 st.text("QR Code:")
-st.image(image)
+st.image(image,caption="Right Click and then Save Image As...")
